7276425bcbe970b7b77fb5fd633b9047e51bd100
martasumyk <116710765+martasumyk@users.noreply.github.com>
2023-05-15 20:20:27 +0300

    Update maze.py

--- maze.py
+++ maze.py
@@ -59,6 +59,7 @@ class Maze:
             current_cell = stack.peek()
             row = current_cell.row
             col = current_cell.col
+            self._mark_path(row, col)
 
             if self._exit_found(row, col):
                 return True
@@ -66,15 +67,15 @@ class Maze:
             if self._valid_move(row - 1, col):  # Check upward
                 self._mark_path(row - 1, col)
                 stack.push(_CellPosition(row - 1, col))
+            elif self._valid_move(row, col + 1):  # Check right
+                self._mark_path(row, col + 1)
+                stack.push(_CellPosition(row, col + 1))
             elif self._valid_move(row + 1, col):  # Check downward
                 self._mark_path(row + 1, col)
                 stack.push(_CellPosition(row + 1, col))
             elif self._valid_move(row, col - 1):  # Check left
                 self._mark_path(row, col - 1)
                 stack.push(_CellPosition(row, col - 1))
-            elif self._valid_move(row, col + 1):  # Check right
-                self._mark_path(row, col + 1)
-                stack.push(_CellPosition(row, col + 1))
             else:
                 stack.pop()
                 self._mark_tried(row, col)
@@ -87,7 +88,7 @@ class Maze:
             for col in range(self.num_cols()):
                 if (
                     self._maze_cells[row, col] == self.PATH_TOKEN
-                    or self._maze_cells[row, col] or self._maze_cells[row, col] == self.TRIED_TOKEN
+                    or self._maze_cells[row, col] == self.TRIED_TOKEN
                 ):
                     self._maze_cells[row, col] = None
 
@@ -101,19 +102,14 @@ class Maze:
                     maze_str += "_ "
                 else:
                     maze_str += cell + " "
-            maze_str = maze_str.rstrip() + "\n"
-        return maze_str.rstrip()
-
+            maze_str += '\n' if row != self.num_rows() - 1 else ''
+        return maze_str
 
     def _valid_move(self, row, col):
         """Returns True if the given cell position is a valid move."""
-        return (
-            row >= 0
-            and row < self.num_rows()
-            and col >= 0
-            and col < self.num_cols()
-            and self._maze_cells[row, col] is None
-        )
+        return row >= 0 and row < self.num_rows() \
+               and col >= 0 and col < self.num_cols() \
+               and self._maze_cells[row, col] is None
 
     def _exit_found(self, row, col):
         """Helper method to determine if the exit was found."""
@@ -136,56 +132,4 @@ class _CellPosition(object):
         self.col = col
 
 
-def build_maze(filename):
-    """Builds a maze based on a text format in the given file."""
-    infile = open(filename, "r")
 
-    # Read the size of the maze.
-    nrows, ncols = read_value_pair(infile)
-    maze = Maze(nrows, ncols)
-
-    # Read the starting and exit positions.
-    row, col = read_value_pair(infile)
-    maze.set_start(row, col)
-    row, col = read_value_pair(infile)
-    maze.set_exit(row, col)
-
-    # Read the maze itself.
-    for row in range(nrows):
-        line = infile.readline()
-        for col in range(len(line)):
-            if line[col] == "*":
-                maze.set_wall(row, col)
-
-    # Close the maze file and return the newly constructed maze.
-    infile.close()
-
-    return maze
-
-
-def read_value_pair(infile):
-    """Extracts an integer value pair from the given input file."""
-    line = infile.readline()
-    valA, valB = line.split()
-    return int(valA), int(valB)
-
-maze = build_maze("mazefile.txt")
-print(maze.find_path())
-print(maze)
-print(len(str(maze)))
-print(str(maze) == '* * * * * \
-* o * o * \
-* x x x * \
-* x * x x \
-_ x * * * ')
-# * * * * *
-# * o * o *
-# * x x x *
-# * x * x x
-# _ _ * * *
-
-# * * * * * 
-# * o * o * 
-# * x x x * 
-# * x * x x 
-# _ x * * * 
